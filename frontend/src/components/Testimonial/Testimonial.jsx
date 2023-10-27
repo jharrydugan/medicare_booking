2d4ff526a7cb9232c0a14507edf7104a1e93260b
JHARRYDUGAN <jharrydugan@gmail.com>
2023-10-27 03:43:38 -0700

    first commit

---
+++ frontend/src/components/Testimonial/Testimonial.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import {Pagination} from 'swiper';
+import {Swiper, SwiperSlide} from 'swiper/react';
+import 'swiper/css'
+import 'swiper/css/pagination'
+import patientAvatar from '../../assets/images/patient-avatar.png';
+
+const Testimonial = () => {
+  return (
+    <div>Testimonial</div>
+  )
+}
+
+export default Testimonial
